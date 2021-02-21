f2f12cc1d2c9bed1434de27b81816d26f9acd2d8
Daniel Acuña <daniel.leonardoacua@gmail.com>
2021-02-20 20:56:26 -0600

    Actualizacion/Subir el avatar del usuario
    El usuario ya puede subir su foto de perfil, 0 errores detectados.

--- client/src/app/components/user_edit.component.ts
+++ client/src/app/components/user_edit.component.ts
@@ -1,5 +1,6 @@
 import { ImportResolver } from '@angular/compiler';
 import {Component, OnInit} from '@angular/core';
+import {GLOBAL} from '../services/global';
 import {UserService} from '../services/user.service';
 import { User } from "../models/user";
 import { Console } from 'console';
@@ -16,6 +17,7 @@ export class UserEditComponent implements OnInit{
     public identity;
     public token;
     public alertMessage;
+    public url:string;
 
     constructor(
         private _userService: UserService
@@ -24,6 +26,7 @@ export class UserEditComponent implements OnInit{
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
     this.user = this.identity;
+    this.url = GLOBAL.url;
     }
 
     ngOnInit(){
@@ -41,7 +44,20 @@ export class UserEditComponent implements OnInit{
                     //this.user = response.user;
                     localStorage.setItem('identity', JSON.stringify(this.user));
                     document.getElementById("identity_name").innerHTML = this.user.name;
+                    
 
+                    if(!this.filesToUpload){
+                        //redireccion
+                    }else{
+                        this.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload).then(
+                            (result: any) => {
+                                this.user.image = result.image;
+                                localStorage.setItem('identity', JSON.stringify(this.user));
+                                console.log(this.user);
+                            }
+                        );
+
+                    }
                     this.alertMessage = 'El usuario se ha actualizado correctamente';
                 }
             },
@@ -54,6 +70,39 @@ export class UserEditComponent implements OnInit{
                 }
             }
         );
+    }
+
+    public filesToUpload: Array<File>;
+
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        console.log(this.filesToUpload);
+
+    }
+
+    makeFileRequest(url: string, params: Array<string>, files: Array<File>){
+        var token = this.token;
+
+        return new Promise(function(resolve,reject){
+            var formData:any = new FormData();
+            var xhr = new XMLHttpRequest();
+
+            for(var i = 0; i < files.length; i++){
+                formData.append('image', files[i], files[i].name);
+            }
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                        resolve(JSON.parse(xhr.response));
+                    }else{
+                        reject(xhr.response);
+                    }
+                }
+            }
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Authorization',token);
+            xhr.send(formData);
+        });
     }
 
 }
