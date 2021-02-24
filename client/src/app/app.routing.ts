362bc89a0d847a10db26f67d7b11605c9876365e
Daniel Acuña <daniel.leonardoacua@gmail.com>
2021-02-23 18:18:10 -0600

    Actualizacion / Home
    Error_conflicto

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,20 +2,17 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // import users
+import { HomeComponent } from './components/home.component';
 import {UserEditComponent} from './components/user-edit.component';
 //import artist
 import { ArtistListComponent } from './components/artist-list.component';
 
 const appRoutes: Routes = [
-    {
-        path:'',
-        redirectTo: '/artists/1',
-        pathMatch: 'full'
-    },
-    {path: '', component: ArtistListComponent},
+
+    {path: '', component: HomeComponent},
     {path: 'artists/:page', component: ArtistListComponent},
     {path: 'mis-datos', component: UserEditComponent},
-    {path: '**', component: ArtistListComponent},
+    {path: '**', component: HomeComponent},
 ];
 
 export const appRoutingProviders: any[] = [];
