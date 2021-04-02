a05f0bbae0fd5be7d27c277a50f89335dae2311d
Axel Malvaez <axelmalvaez07@.com>
2021-04-02 17:59:37 -0600

    Actualización / Componente para editar albums
    Se agregaron las funciones para editar el album, también se agregaron nuevas rutas para el editado de los albums y se corrigieron errores de html

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ArtistAddComponent } from './components/artist-add.component';
 import { ArtistEditComponent } from './components/artist-edit.component';
 import { ArtistDetailComponent } from './components/artist-detail.component';
 import { AlbumAddComponent } from './components/album-add.component';
+import { AlbumEditComponent } from './components/album-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { AlbumAddComponent } from './components/album-add.component';
     ArtistAddComponent,
     ArtistEditComponent,
     ArtistDetailComponent,
-    AlbumAddComponent
+    AlbumAddComponent,
+    AlbumEditComponent
   ],
   imports: [
     BrowserModule,
