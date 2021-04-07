b7cf8312818a60058e478cc8d952f2904488d98b
Daniel Acuña <daniel.leonardoacua@gmail.com>
2021-04-07 00:29:28 -0500

    Actualizacion / El player 116 y 117
    se añaden los compnentes y estilos para el player

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,19 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders} from './app.routing';
 
+//general
 import { AppComponent } from './app.component';
-
 import { HomeComponent } from './components/home.component';
+
+//user
 import { UserEditComponent } from './components/user-edit.component';
+
+//artist
 import { ArtistListComponent } from './components/artist-list.component';
 import { ArtistAddComponent } from './components/artist-add.component';
 import { ArtistEditComponent } from './components/artist-edit.component';
 import { ArtistDetailComponent } from './components/artist-detail.component';
+
+//album
 import { AlbumAddComponent } from './components/album-add.component';
 import { AlbumEditComponent } from './components/album-edit.component';
 import { AlbumDetailComponent } from './components/album-detail.component';
-import { SongAddComponent } from './components/song-add.component';
 
+//song
+import { SongAddComponent } from './components/song-add.component';
+import { PlayerComponent } from './components/player.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +38,8 @@ import { SongAddComponent } from './components/song-add.component';
     AlbumAddComponent,
     AlbumEditComponent,
     AlbumDetailComponent,
-    SongAddComponent
+    SongAddComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
