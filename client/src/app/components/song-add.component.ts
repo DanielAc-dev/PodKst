fcf82ebbf19179b228b5db79df7f5610676bb0ae
JoseMiguel <josemr3002@gmail.com>
2021-04-11 15:13:45 -0500

    Actualización / Redireccion al crear un tema
    Vid 111, re-dirección al añadir una canción

--- client/src/app/components/song-add.component.ts
+++ client/src/app/components/song-add.component.ts
@@ -54,7 +54,7 @@ export class SongAddComponent implements OnInit{
                         this.alertMessage = 'La cancion se ha creado correctamente';
                         this.song = response.song;
                         
-                        //this._router.navigate(['/editar-album', response.album._id]);
+                        this._router.navigate(['/editar-tema', response.song._id]);
                     }
                 },
                 error =>{
