8dfa6a906f5d43abc6e139ff9442d6aaf8429899
JoseMiguel <josemr3002@gmail.com>
2021-04-11 15:26:10 -0500

    Actualización / Servicio de Canciones terminado
    Vid 112, servicio de canciones terminado

--- client/src/app/services/song.service.ts
+++ client/src/app/services/song.service.ts
@@ -13,7 +13,22 @@ export class SongService{
     constructor(private _http: Http){
         this.url = GLOBAL.url;
     }
+    getSongs(token, albumId = null){
+        let headers = new Headers({
+            'Content-Type':'application/json',
+            'Authorization':token
+        });
+        let options = new RequestOptions({headers: headers});
 
+        if(albumId==null){
+            return this._http.get(this.url+'songs', options)
+                .map(res=> res.json());
+        }else{
+            return this._http.get(this.url+'songs/'+albumId, options)
+                .map(res=> res.json());
+        }
+    }
+    
     getSong(token, id: string){
         let headers = new Headers({
             'Content-Type':'application/json',
@@ -71,6 +86,16 @@ export class SongService{
         });
         return this._http.put(this.url+'song/'+id,params,{headers:headers})
                          .map(res => res.json());
+    }
+    deleteSong(token, id: string){
+        let headers = new Headers({
+            'Content-Type':'application/json',
+            'Authorization':token
+        });
+
+        let options = new RequestOptions({headers: headers});
+        return this._http.delete(this.url+'song/'+id, options)
+                         .map(res=> res.json());
     }
     /*
     editAlbum(token,id:string, album: Album){
